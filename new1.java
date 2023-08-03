afb896b63dbed7bcc46747fc0c1e3ba45838b754
Pravinda Bandara <bandarapravinda@gmail.com>
2023-08-03 23:48:38 +0530

    1st commit

---
+++ new1.java
@@ -0,0 +1,10 @@
+public class new1 {
+    public static void main(String[] args) {
+        int x = 5;
+int result = x++; // result is assigned the value of x (5) before incrementing x to 6
+System.out.println("Result: " + result); // Output: Result: 5
+System.out.println("x: " + x); // Output: x: 6
+System.out.println("Result: " + result); 
+System.out.println("x: " + x); // Output: x: 6
+    }
+}
